d193def94775f71376684d5da7c560abcd7daa45
Alex Bakaev <bakaev_a1998@mail.ru>
2025-12-20 13:07:18 +0500

    4-order-api Configured logrus

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"github.com/ANB98prog/purple-school-homeworks/4-order-api/configs"
-	"github.com/ANB98prog/purple-school-homeworks/4-order-api/pkg/db"
+	"github.com/ANB98prog/purple-school-homeworks/4-order-api/pkg/logging"
 )
 
 func main() {
-	conf := configs.LoadConfig()
-	_ = db.NewDb(&conf.Db)
+	configs.ReadEnvironmentVariables()
+	//conf := configs.LoadConfig()
+	config, err := logging.ReadLogConfig()
+	if err != nil {
+		panic(err)
+	}
+	logger := logging.ConfigureLogrus(config)
+
+	//_ = db.NewDb(&conf.Db)
+
+	logger.Info("First logging message")
 }
